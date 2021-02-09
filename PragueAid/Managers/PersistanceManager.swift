5e3eca0de5d1763a8c1d14ff4dd74ea87cf84693
slechtd <dan.slechta@gmail.com>
2021-02-09 16:48:52 +0100

    implement filterSettings persistance

---
+++ PragueAid/Managers/PersistanceManager.swift
@@ -0,0 +1,9 @@
+//
+//  PersistanceManager.swift
+//  PragueAid
+//
+//  Created by Daniel Šlechta on 09.02.2021.
+//  Copyright © 2021 Daniel Šlechta. All rights reserved.
+//
+
+import Foundation
---
+++ PragueAid/Model/FilterSettings.swift
@@ -0,0 +1,9 @@
+//
+//  FilterSettings.swift
+//  PragueAid
+//
+//  Created by Daniel Šlechta on 09.02.2021.
+//  Copyright © 2021 Daniel Šlechta. All rights reserved.
+//
+
+import Foundation
